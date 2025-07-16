8ab15896c4ad8ac2b1e5bfc839e8737fcc418e50
lovelyn2001 <lovelynikwuka2001@gmail.com>
2025-07-16 10:45:33 +0100

    deleted unneccessary folders.

--- lib/data/questions.dart
+++
@@ -1,6 +0,0 @@
-import 'package:xylophone_app/models/questions_schema.dart';
-
-List<Questions> questions = [
-  Questions('What is the capital of France?', false),
-  Questions('What is 2 + 2?', true),
-];
--- lib/models/questions_schema.dart
+++
@@ -1,5 +0,0 @@
-class Questions {
-  const Questions(this.questionText, this.answer);
-  final String questionText;
-  final bool answer;
-}
